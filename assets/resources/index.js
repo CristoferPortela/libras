6035ad813208d280ad6d76631f9b7bdc1cc6a206
Cristofer <cristoferportela@gmail.com>
2019-11-17 23:52:32 -0300

    setInterval function added

--- assets/resources/index.js
+++ assets/resources/index.js
@@ -2,3 +2,20 @@
 import '../libraries/lizFramework/css/generals/liz.css'
 import './app.less'
 import './components'
+import './mainPage'
+
+
+let tooMuchTime = setInterval(() => {
+    confirm('Olá? Ainda está ai?')
+}, 1000*90)
+
+document.addEventListener('scroll', () => {
+    clearInterval(tooMuchTime)
+
+    tooMuchTime = setInterval(() => {
+        confirm('Olá? Ainda está ai?')
+    }, 1000*90)
+})
+
+
+
