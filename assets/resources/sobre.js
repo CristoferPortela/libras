cf956320a3b471d3156c1eaa31fc3975c66db7d6
Cristofer <cristoferportela@gmail.com>
2019-11-30 20:03:47 -0300

    ajax call and inserting elements is inside ajax function

--- assets/resources/sobre.js
+++ assets/resources/sobre.js
@@ -1,8 +1,9 @@
 'use strict'
 import $ from 'jquery'
 
-function ajax() {
+async function ajax() {
     $.get('http://localhost:80/sobre', (data) => {
+        data = JSON.parse(data)
         $('#sobre h2').text(data.sobre.titulo)
         $('#sobre .content p').text(data.sobre.conteudo)
     })
