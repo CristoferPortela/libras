704718d95b7f94a3435c547fa5808f6f11465957
Cristofer <cristoferportela@gmail.com>
2019-11-30 20:02:52 -0300

    checking if has email registered now is son mais.js instead of validation.js

--- assets/resources/mais.js
+++ assets/resources/mais.js
@@ -1,26 +1,29 @@
 'use strict'
+import validateEmail from './validation'
 
 const mais = () => {
-    const validateEmail = email => {
-        if (email !== null & email !== '' && email.match(/^[\W\w]+@{1}\w+(\.[\w]+((\.[\w+]+)+)?)$/)) {
-            localStorage.email = email
-        }
-    }
-    if (localStorage.email === null || localStorage.email === '' || localStorage.email === undefined) {
-        // precisa se ronload, caso a pagina seja carregada para não ficar tudo branco
+
+    if (localStorage.email === null || localStorage.email === '' || localStorage.email === undefined || localStorage.email === 'null') {
+        
+        // precisa ser onload, caso a pagina seja carregada para não ficar tudo branco
         window.addEventListener('load', () => {
             let email = prompt('Gostaria de receber novidades em seu emai? Digite ele abaixo!')
-            validateEmail(email)
+            if (validateEmail(email))
+                localStorage.email = email
         })
+
         window.addEventListener('hashchange', () =>{
+            console.log(this)
             if (location.hash === '#/mais') {
                 let email = prompt('Gostaria de receber novidades em seu emai? Digite ele abaixo!')
-                validateEmail(email)
+                if (validateEmail(email))
+                localStorage.email = email 
             }
         })
     } 
-    if (localStorage.email !== null && localStorage.email !== undefined)
+    if (localStorage.email !== null && localStorage.email !== undefined && localStorage.email !== 'null') {
+        
         document.getElementsByTagName('strong')[0].textContent = 'Muito obrigado pelo interesse'
-    
+    }    
 }
 export default mais
