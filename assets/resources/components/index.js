5e8523e58d7434f096c64309d01f2f46594076c7
Cristofer <cristoferportela@gmail.com>
2019-11-30 19:52:12 -0300

    swipe to next page function added

--- assets/resources/components/index.js
+++ assets/resources/components/index.js
@@ -6,23 +6,69 @@ import './Sobre.less'
 import $ from 'jquery'
 import htmlMenu from './Menu.html'
 import htmlFooter from './Footer.html'
-import routes from '../routes'
+import { routing } from '../routes'
+import { hashing } from '../routes'
+import 'jquery-touchswipe'
 
 (function() {
     $('body').prepend(htmlMenu)
     $('script').before(htmlFooter)
 
-    routes()
+    routing()
 
-    window.addEventListener('hashchange', () => routes())
+    window.addEventListener('hashchange', () => routing())
+    window.addEventListener('hashchange', (e) => { 
+        window.scrollTo(0,0);
+        $('#guias').html(`<a href="${e.oldURL}" class="teri-gaki-persimmon">Voltar página anterior {${e.oldURL}}</a>`)
+    })
 
+    const hashIndex = () => {
+        for (let i in hashing) {
+            if (location.hash === hashing[i].nome)
+                return Number(i)
+        }
+    }
+
+    let index = hashIndex()
+
+    document.addEventListener('keydown', async e => {
+        if (e.keyCode === 39) {
+            index += 1
+            if (index >= hashing.length)
+                index = 0
+            location.hash = hashing[index].nome
+        }
+    })
+    document.addEventListener('keydown', async e => {
+        if (e.keyCode === 37) {
+            index -= 1
+            if (index < 0)
+                index = hashing.length - 1
+            location.hash = hashing[index].nome
+        }
+    })
+
+    $('body').swipe({
+        swipe:function(event, direction, distance) {
+            if (direction === 'left' && distance >= 150) {
+                index += 1
+                if (index >= hashing.length)
+                    index = 0
+                location.hash = hashing[index].nome
+            } else if (direction === 'right' && distance >= 150) {
+                index -= 1
+                if (index < 0)
+                    index = hashing.length - 1
+                location.hash = hashing[index].nome
+            }
+        }
+    })
 })()
 
 // The menu action on clicking it
 
 const change = (changed, class1, class2) => {
     let toChange = document.querySelector(changed)
-
     if (toChange.classList.contains(class1)) {
         toChange.classList.remove(class1)
         toChange.classList.add(class2)
